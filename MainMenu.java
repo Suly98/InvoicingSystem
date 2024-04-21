237fca4abbce6417c8add10a2f1e39ee40c1f626
Suly98 <135811486+Suly98@users.noreply.github.com>
2024-04-21 09:54:46 +0400

    added a new class

---
+++ MainMenu.java
@@ -0,0 +1,10 @@
+package InvoiceSystem;
+
+public class MainMenu {
+    public static void main(String[] args){
+        StringBuffer end = new StringBuffer();
+        while (end.equals("exit")){
+            
+        }
+    }
+}
