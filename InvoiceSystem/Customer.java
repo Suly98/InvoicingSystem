7a63f24f7689a9246e09bf2723a1b210b140ab52
Suly98 <135811486+Suly98@users.noreply.github.com>
2024-05-01 10:01:47 +0400

    added Customer interface

--- InvoiceSystem/Customer.java
+++ InvoiceSystem/Customer.java
@@ -2,7 +2,7 @@ package InvoiceSystem;
 
 import java.util.Objects;
 
-public class Customer {
+public class Customer implements customerInterface {
     Integer id;
     String fullName;
     String phoneNo;
---
+++ InvoiceSystem/customerInterface.java
@@ -0,0 +1,7 @@
+package InvoiceSystem;
+
+public interface customerInterface {
+    public void setId(Integer id);
+    public void setFullName(String fullName);
+    public void setPhoneNo(String phoneNumber);
+}
